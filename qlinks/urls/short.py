42b39ca2ca8ab932fa0ff7f3364236944057df28
Quantum <quantum2048@gmail.com>
2022-01-23 23:38:45 -0500

    Handle root redirect

--- qlinks/urls/short.py
+++ qlinks/urls/short.py
@@ -1,7 +1,10 @@
+from functools import partial
+
 from django.urls import path
 
 from qlinks.views import short_link
 
 urlpatterns = [
+    path('', partial(short_link, slug=''), name='short_link'),
     path('<slug>', short_link, name='short_link'),
 ]
