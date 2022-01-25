44d5e647398f8826a641ff53604aac50fb90ead1
Quantum <quantum2048@gmail.com>
2022-01-24 20:06:54 -0500

    Handle failing health checks

--- qlinks/health.py
+++ qlinks/health.py
@@ -1,5 +1,13 @@
+import logging
+
 import requests
+
+logger = logging.getLogger(__name__)
 
 
 def check_url(url):
-    return 200 <= requests.get(url).status_code < 400
+    try:
+        return 200 <= requests.get(url).status_code < 400
+    except requests.RequestException:
+        logger.exception('Failed to fetch: %s', url)
+        return False
