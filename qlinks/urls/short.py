1a60694112a709312b801c2435b249513efdca5d
Quantum <quantum2048@gmail.com>
2025-01-02 01:08:10 -0500

    Add health check endpoint

--- qlinks/urls/short.py
+++ qlinks/urls/short.py
@@ -2,9 +2,10 @@ from functools import partial
 
 from django.urls import path
 
-from qlinks.views import short_link
+from qlinks.views import health_check, short_link
 
 urlpatterns = [
     path('', partial(short_link, path=''), name='short_link'),
+    path('api/health_check', health_check, name='health_check'),
     path('<path:path>', short_link, name='short_link'),
 ]
--- qlinks/views.py
+++ qlinks/views.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 
 from qlinks.models import Link
@@ -10,3 +10,8 @@ def short_link(request, path):
     return HttpResponseRedirect(link, headers={
         'X-Powered-By': settings.QLINKS_POWERED_BY
     })
+
+
+def health_check(request):
+    Link.objects.exists()
+    return HttpResponse('qlinks seems to work!')
